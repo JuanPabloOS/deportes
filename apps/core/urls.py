8a3a934610ccc1cec879e748ea7e4c641ab36687
Juan Olvera <jp.oes@hotmail.com>
2020-05-19 19:51:19 -0500

    Implementar auth

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home_view, name="home"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
     path('profile/', views.profile_view, name="profile"),
     path('changePassword/', views.changePassword_view, name="changePassword")
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from apps.team.models import Team
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
 # Create your views here.
 def home_view(request):
-    return render(request, 'home.html')
+    teams = Team.objects.all()
+    return render(request, 'home.html', {'teams':teams})
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Binevenido")
+        else:
+            return render(request, 'login.html', {'form':form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form':form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponse("Hasta luego")
 
 def profile_view(request):
     return render(request, 'profile.html')
